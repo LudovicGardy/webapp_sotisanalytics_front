096f9882f7f3e0deb9a17c2b9761e11019cf2408
Ludovic Gardy <ludovic.gardy@gmail.com>
2025-04-14 19:25:43 +0200

    Refactor Contact component structure for improved readability and maintainability

--- src/pages/Contacts/Contact.js
+++ src/pages/Contacts/Contact.js
@@ -1,25 +1,28 @@
 import React from "react";
 
+// Styles globaux
 import "../../App.css";
-import Footer from "../../components/Footer/Footer";
-import Contact_Banner from "./Contacts_banner";
 
+// Composants globaux
+import Footer from "../../components/Footer/Footer";
+
+// Composants spécifiques Contact
+import Contact_Banner from "./Contacts_banner";
 import Contact_Field from "./Contacts_field";
 import GoogleApiWrapper from "./Google_maps";
 import ContactInfo from "./Contacts_info";
 // import Contact_Schedule from "./Contact/Contact_Schedule";
 
-
 function Contact() {
   return (
-    <>
-        <Contact_Banner/>
-        {/* <Contact_Schedule/> */}
-        <Contact_Field/>
-        {/* <ContactInfo/> */}
-        <GoogleApiWrapper/>
-        <Footer/>
-    </>
+    <main className="contact-page">
+      <Contact_Banner />
+      {/* <Contact_Schedule /> */}
+      <Contact_Field />
+      {/* <ContactInfo /> */}
+      <GoogleApiWrapper />
+      <Footer />
+    </main>
   );
 }
 
