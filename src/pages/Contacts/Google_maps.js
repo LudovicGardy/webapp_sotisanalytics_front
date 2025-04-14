9fb11dd6ff8ec1fe7c576d923a67b237420c4485
Ludovic Gardy <ludovic.gardy@gmail.com>
2025-04-14 20:04:07 +0200

    Refactor Google Maps component and CSS for improved readability and structure

--- src/pages/Contacts/Google_maps.js
+++ src/pages/Contacts/Google_maps.js
@@ -1,126 +1,72 @@
-import React, {Component} from "react";
+import React from "react";
 import "./Google_maps.css";
+import { Map, Marker, GoogleApiWrapper } from "google-maps-react";
 
-import { Fade } from 'react-reveal';
-
-import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
-
- 
-const apiKey = 'AIzaSyDPxWsYNUh76-tPtsnONUQ8I0eSu-ZkSco';
+// API Key Google Maps
+const apiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
 
 const mapStyles = {
-    width: "100%",
-    height: "100%",
+  width: "100%",
+  height: "100%",
+};
+
+const greyStyle = [
+  {
+    featureType: "landscape",
+    stylers: [{ saturation: -100 }, { lightness: 65 }, { visibility: "on" }],
+  },
+  {
+    featureType: "poi",
+    stylers: [{ saturation: -100 }, { lightness: 51 }, { visibility: "simplified" }],
+  },
+  {
+    featureType: "road.highway",
+    stylers: [{ saturation: -100 }, { visibility: "simplified" }],
+  },
+  {
+    featureType: "road.arterial",
+    stylers: [{ saturation: -100 }, { lightness: 30 }, { visibility: "on" }],
+  },
+  {
+    featureType: "road.local",
+    stylers: [{ saturation: -100 }, { lightness: 40 }, { visibility: "on" }],
+  },
+  {
+    featureType: "administrative.province",
+    stylers: [{ visibility: "off" }],
+  },
+  {
+    featureType: "water",
+    elementType: "labels",
+    stylers: [{ lightness: -25 }, { saturation: -100 }, { visibility: "on" }],
+  },
+];
+
+function MapContainer({ google }) {
+  const onMapClicked = () => {
+    console.log("Map clicked");
   };
 
-  const greyStyle = [
-    {
-      featureType: "landscape",
-      stylers: [
-        { saturation: -100 },
-        { lightness: 65 },
-        { visibility: "on"},
-      ],
-    },
-
-    {
-      featureType: "poi",
-      stylers: [
-        { saturation: -100 },
-        { lightness: 51 },
-        { visibility: "simplified"},
-      ]
-    },
-
-
-    {
-      featureType: "road.highway",
-      stylers: [
-        { saturation: -100 },
-        { visibility: "simplified"},
-      ]
-    },
-
-    {
-      featureType: "road.arterial",
-      stylers: [
-        { saturation: -100 },
-        { lightness: 30 },
-        { visibility: "on"},
-      ]
-    },      
-
-    {
-      featureType: "road.local",
-      stylers: [
-        { saturation: -100 },
-        { lightness: 40 },
-        { visibility: "on"},
-      ]
-    },
-
-    {
-      featureType: "administrative.province",
-      stylers: [
-        { visibility: "off"},
-      ]
-    },
-
-    {
-      featureType: "water",
-      elementType: "labels",
-      stylers: [
-        { lightness: -25 },
-        { saturation: -100 },
-        { visibility: "on"},
-      ]
-    },
-
-    // {
-    //   featureType: "water",
-    //   elementType: "geometry",
-    //   stylers: [
-    //     { hue : "#ffff00" },
-    //     { lightness: -25 },
-    //     { saturation: -97 },
-    //   ]
-    // },
-  ];
-
-
-function MapContainer(props) {
-
-function onMapClicked() {
-    // fonction de gestion de l'événement onClick
-}
-
-
-return (
-    <>
-    
+  return (
     <div className="gmap">
-        <Map
-            google={props.google}
-            zoom={14}
-            style={mapStyles}
-            styles={greyStyle}
-            initialCenter={{
-            lat: 48.883577,
-            lng: 2.2694351,
-            }}
-            onClick={onMapClicked}
-        >
-            <Marker
-            title={'Purpan Hospital'}
-            name={'Purpan Hospital'}
-            position={{ lat: 43.695653, lng: 7.287631 }}
-            />
-        </Map>
+      <Map
+        google={google}
+        zoom={14}
+        style={mapStyles}
+        styles={greyStyle}
+        initialCenter={{ lat: 48.883577, lng: 2.2694351 }}
+        onClick={onMapClicked}
+      >
+        <Marker
+          title="Purpan Hospital"
+          name="Purpan Hospital"
+          position={{ lat: 43.695653, lng: 7.287631 }}
+        />
+      </Map>
     </div>
-    </>
-);
+  );
 }
 
-export default GoogleApiWrapper({
-  apiKey: (apiKey)
-})(MapContainer)
+const WrappedMap = GoogleApiWrapper({ apiKey })(MapContainer);
+
+export default WrappedMap;
