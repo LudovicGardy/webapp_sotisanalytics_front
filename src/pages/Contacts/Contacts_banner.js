76c29182e7c1d696e01536e625b7a431378261b9
Ludovic Gardy <ludovic.gardy@gmail.com>
2025-04-14 19:28:52 +0200

    Refactor ContactBanner component for improved structure and readability

--- src/pages/Contacts/Contacts_banner.js
+++ src/pages/Contacts/Contacts_banner.js
@@ -1,19 +1,19 @@
-import React, { useState } from "react";
+import React from "react";
 import "./Contacts_banner.css";
 
 function ContactBanner() {
-    return (
-        <>
-        <div className="contact-container">
+  return (
+    <div className="contact-container">
+      <div className="services-banner">
+        <img
+          src="/images/LG_banners/contact/format_L_v2.jpg"
+          alt="Contact banner"
+          loading="lazy"
+        />
+        <h1 className="banners-title">CONTACT</h1>
+      </div>
+    </div>
+  );
+}
 
-          <div className="services-banner">
-              <img src="/images/LG_banners/contact/format_L_v2.jpg"></img>
-              <h1 className= "banners-title">CONTACT</h1>
-          </div>
-
-        </div>
-        </>
-      );
-    }
-  
-  export default ContactBanner;
+export default ContactBanner;
